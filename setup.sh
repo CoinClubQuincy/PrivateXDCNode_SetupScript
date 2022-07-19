e9e230cbfc5788c617cdab80f1163bbb435c12ba
Q <rqjones@asu.edu>
2022-07-18 19:09:28 -0700

    check amount of nodes & add last dependancies

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # install python and python dpendancies
 #yes |
 location=$(pwd)
@@ -10,7 +12,10 @@ sudo apt-get update
 sudo apt-get update -y
 sudo apt-get update --fix-missing
 sudo apt-get install make
-
+#bootload dependancies
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install bootnode
 #remove olg Go
 yes | sudo apt-get remove golang-go
 yes | sudo apt-get remove --auto-remove golang-go
@@ -54,20 +59,35 @@ cd /home/ubuntu/xdc-testnetwork-leo/genesis
 #Delete the existing Genesis file
 rm -rf genesis.json
 
-echo $puppeth
+
 #run python script to set configuration parameters
 echo "finnished Dependencies"
 echo "Start python Script"
 
-python3 $location/config.py $location
-
-
-alias bootnode=/home/ubuntu/XDPoSChain/build/bin/bootnode
+echo "2/3rd complete: BREAK: still testing finnish manually until finnished"
+break ## still testing do manually untill finnished
+nodeAmmount = $(python3 $location/config.py $location)
+echo "$nodeAmmount"
 
 bootnode -genkey bootnode.key
 bootnode -nodekey ./bootnode.key
 
-#Note - Make sure to add Public ip in enode-id (For connecting two different location nodes you should have to go with aws public ip)
+#Set up master node
+for (( i=1; i<=$nodeAmmount; i++ ))
+do 
+    echo "Setting up Node $i"
+    chmod 777 node$i.sh
+    bash node$i.sh
+done
+
+#Set up Faucet Backend
+git clone https://github.com/XDCFoundation/xdc-faucet-leo-backend.git
+npm install
+npm run dev
+
+#Set up Faucet Frontend
+git clone https://github.com/XDCFoundation/xdc-faucet-leo.git
+npm start
 
 echo "Node Setup Completed"
 
